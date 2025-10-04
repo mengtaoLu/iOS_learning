4a022f2b3acca1b1c4b6d13156c2105d04580ec3
taogg123 <lumengtao@live.com>
2025-10-04 23:36:33 +0800

    update

---
+++ navigator_sample_data/FriendsFavoriteMovies/FriendsFavoriteMovies/Friend.swift
@@ -0,0 +1,18 @@
+//
+//  Friends.swift
+//  FriendsFavoriteMovies
+//
+//  Created by 陆梦涛 on 2025/9/30.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Friends {
+    var name:String
+    
+    init(name:String) {
+        self.name = name
+    }
+}
