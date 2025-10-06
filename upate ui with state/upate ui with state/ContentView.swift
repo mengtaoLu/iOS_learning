532e1e0410839efaca5cf093c67860bdc09ff993
taogg123 <lumengtao@live.com>
2025-10-06 22:45:06 +0800

    @state

---
+++ upate ui with state/upate ui with state/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//ContentView.swift
+//upate ui with state
+//
+//Created by Mengtao on 2025/10/5.
+//Copyright (c) 2025 .
+  
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ upate ui with state/upate ui with state/upate_ui_with_stateApp.swift
@@ -0,0 +1,18 @@
+//
+//upate_ui_with_stateApp.swift
+//upate ui with state
+//
+//Created by Mengtao on 2025/10/5.
+//Copyright (c) 2025 .
+  
+
+import SwiftUI
+
+@main
+struct upate_ui_with_stateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
